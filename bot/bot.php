211f483ddb88bbd38388e1c195b77d07f5496d2a
rluts <rluts@yandex.ua>
2014-07-12 14:22:23 +0300

    comments

--- bot/bot.php
+++ bot/bot.php
@@ -22,8 +22,8 @@ foreach($regions as $region) {
 	$content = $bot->getpage($page);
 	#echo $content;
 
+	//Цю частину потрібно було б переписати
 	preg_match_all('/\{\{ВЛЗ-рядок\s*?\|\s*?ID\s*?=\s*?([0-9]{1,2})-([0-9]{1,3})-([0-9]{1,4})\s*?\|\s*?назва\s*?=\s*?(.*?)\s*?\|\s*?постанова\s*?=\s*?(.*?)\s*?\|\s*?розташування\s*?=\s*?(.*?)\s*?\|\s*?користувач\s*?=\s*?(.*?)\s*?\|\s*?площа\s*?=\s*?(.*?)\s*?\|\s*?широта\s*?=\s*?(.*?)\s*?\|\s*?довгота\s*?=\s*?(.*?)\s*?\|\s*?тип\s*?=\s*?(.*?)\s*?\|\s*?підтип\s*?=\s*?(.*?)\s*?\|\s*?фото\s*?=\s*?(.*?)\s*?\|\s*?галерея\s*?=\s*?(.*?)\s*?}}/', $content, $matches);
-
 	$array = array();
 	foreach($matches[0] as $key=>$value) {
 		$array[$key]['id'] = $connection->escape_string(trim($matches[1][$key])) . $connection->escape_string(trim($matches[2][$key])) . $connection->escape_string(trim($matches[3][$key]));
@@ -40,7 +40,6 @@ foreach($regions as $region) {
 		$array[$key]['galery'] = $connection->escape_string(trim($matches[14][$key]));
 		
 		$query = "INSERT INTO list VALUES('{$array[$key]['id']}', '$region', '{$array[$key]['title']}', '{$array[$key]['situation']}', '{$array[$key]['owner']}', '{$array[$key]['area']}', '{$array[$key]['lat']}', '{$array[$key]['lon']}', '{$array[$key]['type']}', '{$array[$key]['subtype']}', '{$array[$key]['photo']}', '{$array[$key]['galery']}') ON DUPLICATE KEY UPDATE id='{$array[$key]['id']}', region='$region', title='{$array[$key]['title']}', location='{$array[$key]['situation']}', owner='{$array[$key]['owner']}', area='{$array[$key]['area']}', lat='{$array[$key]['lat']}', longit='{$array[$key]['lon']}', type='{$array[$key]['type']}', subtype='{$array[$key]['subtype']}', photo='{$array[$key]['photo']}', galery='{$array[$key]['galery']}'";
-#		$query = "";
 		echo "$i. $query<br>";
 		$i++;
 		if($connection->query($query)) echo "<span style=\"color:green\">OK</span><br>"; else echo "<span style=\"color:red\">Fail</span>: " . $connection->error . "<br>";
